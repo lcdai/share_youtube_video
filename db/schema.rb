6bba012ff579a2a0924b1061dbc8a6da4d35f38c
Dai Luong <lcongdai@gmail.com>
2022-10-14 08:48:32 +0700

    update our gemfile for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20221012074044) do
 
-  create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "password_digest"
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        limit: 255
+    t.string   "password_digest", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "videos", force: true do |t|
-    t.string   "link"
-    t.string   "title"
+  create_table "videos", force: :cascade do |t|
+    t.string   "link",         limit: 255
+    t.string   "title",        limit: 255
     t.datetime "published_at"
-    t.integer  "likes"
-    t.integer  "dislikes"
-    t.string   "uid"
+    t.integer  "likes",        limit: 4
+    t.integer  "dislikes",     limit: 4
+    t.string   "uid",          limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
